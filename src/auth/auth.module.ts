48970295111d3062e3c64681d7032d529dc02bf9
alekspeacedatta <faleqs33@gmail.com>
2026-02-12 18:42:49 +0400

    added JWT tokens and PasswordHash

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,25 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import type { StringValue } from 'ms';
+
 
 @Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+                    inject: [ConfigService],
+                    useFactory: (config: ConfigService) => ({
+                    secret: config.get<string>('JWT_SECRET')!,
+                    signOptions: {
+                    expiresIn: (config.get<StringValue>('JWT_EXPIRES_IN') ?? '1h') as StringValue,
+                },
+            }),
+        })
+    ],
     controllers: [ AuthController ],
     providers: [ AuthService ],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,49 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, UnauthorizedException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 import { RegisterDto } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-    private users: { email: string, password: string }[] = [];
+    constructor(private readonly jwtService : JwtService) {}
 
-    register( dto : RegisterDto ){
+    private users: { id: string, email: string, passwordHash: string }[] = [];
+
+    async register( dto : RegisterDto ){
         const exsists = this.users.find((u) => u.email === dto.email);
         if(exsists) throw new ConflictException('Email already exsists');
 
-        const user = { email: dto.email, password: dto.password };
+        const passwordHash = await bcrypt.hash(dto.password, 10)
+
+        const user = { id: crypto.randomUUID(), email: dto.email, passwordHash: passwordHash };
         this.users.push(user);
+
+        const access_token = await this.signToken(user.id, user.email);
+        
         return {
             message: 'Successfully Registered',
-            user: { email: user.email }
+            access_token,
+            user: { id: user.id, email: user.email }
         }
     }
-    login( dto : LoginDto ){
-        const matchs = this.users.find((u) => u.email === dto.email)
-        if(!matchs) throw new ConflictException('You are not registered yet');
-        return {
-            message: 'You are logined brother',
-            user: { email: dto.email }
-        }
-    }
+    async login( dto : LoginDto ){
+        const user = this.users.find((u) => u.email === dto.email)
+        if(!user) throw new UnauthorizedException('Invalid Email or Password');
 
+        const ok = await bcrypt.compare(dto.password, user.passwordHash)
+        if(!ok) throw new UnauthorizedException('Invalid Email or Password');
+
+        const access_token = await this.signToken(user.id, user.email);
+
+
+        return {
+            message: 'You are logged in',
+            access_token,
+            user: { id: user.id, email: user.email }
+        }
+    }
+    private signToken(userId: string, email: string) {
+        return this.jwtService.signAsync({ sub: userId, email });
+    }
 }
